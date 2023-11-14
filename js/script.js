73ff8fa53db891023fc71341c986c9b438840d1a
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-11-13 21:31:28 -0300

    finished project

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 import DisplayToday from './modules/DisplayToday.js'
 import TodoList from './modules/ToDoList.js'
 
-const list = document.querySelectorAll('.task')
-
 const displayToday = new DisplayToday('.today')
 displayToday.init()
 
 const todoList = new TodoList('.create-button', '.todo-list')
 todoList.init()
-
-console.log(list.values)
